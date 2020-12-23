0e4f0a1bf9a4817d74fa3124f6cb0be643482090
Feelan <panfilovich1@gmail.com>
2020-12-23 23:07:36 +0300

    1

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
-
-
+import sys
 
 def next():
     input("Нажмите ENTER, чтобы продолжить...")
 
+def openFile():
+    fileName = str(input("Введите имя файла >>> "))
+    try:
+        file = open(fileName, "r", encoding = 'utf-8')
+    except:
+        print("Не получается открыть файл."
+              "\nПрограмма завершает свою работу.")
+        next()
+        sys.exit()
+    else:
+        return file
+
+def welcom():
+    print("\t\t\tДобро пожаловать в игру викторину!")
 
 
-def main ():
+def block(file):
+
+
+
+def main():
+    welcom()
+    file = openFile()
+
+
+
+main()
